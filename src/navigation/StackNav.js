dbadffe041cf92e876a94433d5042e6fdbd44ae3
Anas Hbijan <46601068+Anas-Hb@users.noreply.github.com>
2021-11-10 21:56:00 +0100

    Add files via upload

--- src/navigation/StackNav.js
+++ src/navigation/StackNav.js
@@ -1,1 +1,26 @@
+import React, { Component } from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 
+import Menu from "../Menu/Menu";
+import Game from "../Game/Game";
+
+const Stack = createStackNavigator();
+
+export default class StackNav extends Component {
+  render() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Menu"
+          component={Menu}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Game"
+          component={Game}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    );
+  }
+}
